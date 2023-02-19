7de5cc7bf6a341a291aea4cbdc4bad5ad64074e5
cuzolo <125775406+cuzolo@users.noreply.github.com>
2023-02-19 01:57:51 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -40,8 +40,8 @@ local Library = {
 			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
 		Serika = {
-			Main = Color3.fromRGB(50, 52, 55),
-			Secondary = Color3.fromRGB(80, 82, 85),
+			Main = Color3.fromRGB(138, 43, 226),
+			Secondary = Color3.fromRGB(138, 43, 226),
 			Tertiary = Color3.fromRGB(138, 43, 226),
 
 			StrongText = Color3.fromHSV(138, 43, 226),		
